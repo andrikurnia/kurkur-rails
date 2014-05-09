2cbbac7936e5ed46ca1fe2224009e0a5880370e1
andrikurnia <andrikurnia@live.com>
2014-05-09 16:19:08 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KurkurRails::Application.config.secret_key_base = '7a369d66f95d902733f203c9ea60274aeb416afc1056397fc1b84c1be693b010a9dfa0b864b7223e53a3f4faa836536ea6009bb0035ddeb7ac7997a78e72292e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KurkurRails::Application.config.session_store :cookie_store, key: '_kurkur-rails_session'
